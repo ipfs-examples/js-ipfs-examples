6e9405939b55e9bbc5438e3a912065ee60dde962
Olivério Sousa <47525443+oliveriosousa@users.noreply.github.com>
2021-09-09 11:36:45 +0100

    fix: update service worker test to use playwright (#33)
    * fix: update test to use playwright
    * fix: add retry

---
+++ examples/browser-service-worker/tests/test.js
@@ -0,0 +1,41 @@
+'use strict'
+
+const { test, expect } = require('@playwright/test');
+const { playwright } = require('test-util-ipfs-example');
+
+// Setup
+const play = test.extend({
+  ...playwright.servers(),
+});
+
+play.describe('browser service worker:', () => {
+  // DOM
+  const linkDOM = "a"
+  const textDOM = "body"
+  const debugDOM = "#debug"
+
+  play.beforeEach(async ({servers, page}) => {
+    await page.goto(`http://localhost:${servers[0].port}/`);
+  })
+
+  play('should properly load the content of an IPFS hash', async ({ page }) => {
+    await page.waitForSelector(textDOM)
+    await page.waitForSelector(linkDOM)
+
+    expect(await page.textContent(textDOM)).toContain("Load content by adding IPFS path to the URL")
+    expect(await page.textContent(linkDOM)).toContain("/ipfs/bafy")
+
+    await page.waitForSelector(`${debugDOM}:has-text("SW is ready")`, {
+      state: 'attached'
+    })
+
+    const currentURL = await page.url();
+    await page.goto(`${currentURL}ipfs/Qmf412jQZiuVUtdgnB36FXFX7xg5V6KEbSJ4dpQuhkLyfD`);
+    await page.waitForSelector(textDOM)
+
+    const elementFrame = await page.waitForSelector("iframe")
+    const frame = await elementFrame.contentFrame()
+
+    expect(await frame.textContent('pre')).toContain("hello world")
+  });
+});
