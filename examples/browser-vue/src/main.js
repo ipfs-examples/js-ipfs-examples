2f8130ba6c14e4427ee6b9d4db4e240452a25234
achingbrain <alex@achingbrain.net>
2021-09-23 20:27:51 +0100

    chore: fix ipld test

--- examples/browser-vue/src/main.js
+++ examples/browser-vue/src/main.js
@@ -1,6 +1,6 @@
-import { createApp,  } from 'vue'
+import { createApp } from 'vue'
 import App from './App.vue'
-import VueIpfs from './plugins/vue-ipfs';
+import VueIpfs from './plugins/vue-ipfs'
 
 // Load our IPFS plugin.
 const app = createApp(App)
