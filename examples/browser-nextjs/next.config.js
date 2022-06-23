84bcb3b65cb556f99e674a1a196e9e5d6694b197
achingbrain <alex@achingbrain.net>
2022-06-23 15:18:44 +0100

    chore: fix next test

--- examples/browser-nextjs/next.config.js
+++ examples/browser-nextjs/next.config.js
@@ -1,9 +1,9 @@
-module.exports = {
+export default {
   reactStrictMode: true,
   // https://github.com/vercel/next.js/issues/21079
   // Remove the workaround the issue is fixed
   images: {
     loader: "imgix",
     path: "https://noop/",
-  },
+  }
 }
