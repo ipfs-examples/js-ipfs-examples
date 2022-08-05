0ef940a36d591b7aa92094aafa91642177e0afc1
Umed Khudoiberdiev <pleerock.me@gmail.com>
2022-08-05 21:26:55 +0500

    fix vite example
    as discussed in #4147

--- examples/browser-vite/vite.config.js
+++ examples/browser-vite/vite.config.js
@@ -6,5 +6,12 @@ export default {
   define: {
     'process.env.NODE_DEBUG': 'false',
     'global': 'globalThis'
+  },
+  optimizeDeps: {
+    // target: es2020 added as workaround to make big ints work
+    // https://github.com/vitejs/vite/issues/9062#issuecomment-1182818044
+    esbuildOptions: {
+      target: 'es2020',
+    },
   }
 }
