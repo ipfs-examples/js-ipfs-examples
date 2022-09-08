b2fd45890e4e5d97d1d056e7ea92aef4573ef9a2
Umed Khudoiberdiev <pleerock.me@gmail.com>
2022-09-08 12:49:12 +0500

    fix: update vite example (#431)
    Fixes: #449
    
    Co-authored-by: achingbrain <alex@achingbrain.net>

--- examples/browser-vite/vite.config.js
+++ examples/browser-vite/vite.config.js
@@ -1,10 +1,29 @@
-export default {
+import { defineConfig } from 'vite'
+
+export default defineConfig({
   build: {
-    target: 'esnext',
-    minify: false
+    target: 'es2020',
+    minify: false,
+    // disable @rollup/plugin-commonjs https://github.com/vitejs/vite/issues/9703#issuecomment-1216662109
+    // should be removable with vite 4 https://vitejs.dev/blog/announcing-vite3.html#esbuild-deps-optimization-at-build-time-experimental
+    commonjsOptions: {
+      include: []
+    }
   },
   define: {
     'process.env.NODE_DEBUG': 'false',
     'global': 'globalThis'
+  },
+  optimizeDeps: {
+    // enable esbuild dep optimization during build https://github.com/vitejs/vite/issues/9703#issuecomment-1216662109
+    // should be removable with vite 4 https://vitejs.dev/blog/announcing-vite3.html#esbuild-deps-optimization-at-build-time-experimental
+    disabled: false,
+
+    // target: es2020 added as workaround to make big ints work
+    // - should be removable with vite 4
+    // https://github.com/vitejs/vite/issues/9062#issuecomment-1182818044
+    esbuildOptions: {
+      target: 'es2020'
+    }
   }
-}
+})
