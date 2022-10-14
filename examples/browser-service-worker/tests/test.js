49250ad9d7b976872884bad99853d92b58257e56
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2022-10-14 11:22:03 -0700

    chore: fix CI workflows (#527)
    * chore: tmp
    
    testing to see if changes in #517 break CI workflows, or if they're already broken
    
    * chore: attempt to fix ci runs
    
    see https://playwright.dev/docs/cli\#install-system-dependencies
    
    * fix: github workflow syntax
    
    * chore: ensure concurrency on branch name
    
    * chore: update actions
    
    * fix: don't fail-fast for matrix failures
    
    * chore: attempt to remove GabrielBB/xvfb-action
    
    some steps are showing the following failure on github:
    Node.js 12 actions are deprecated. For more information see: https://github.blog/changelog/2022-09-22-github-actions-all-actions-will-begin-running-on-node16-instead-of-node12/. Please update the following actions to use Node.js 16: GabrielBB/xvfb-action
    
    see https://github.com/SgtPooki/js-ipfs-examples/actions/runs/3245100955
    
    * chore: remove gabriel/xvfb-action for good
    
    * chore: add xvfb-maybe to run-in-electron
    
    * chore: move xvfb-maybe to devDeps
    
    * fix: don't overwrite promises that are being awaited
    
    * chore(tmp): disable all but browser-service-worker tests
    
    * fix(browser-service-worker): move initial page.goto inside test
    
    * fix(browser-service-worker): increase test timeout
    
    * fix(test-util-ipfs-example.servers): stop instead of close
    
    * fix(test-util-ipfs-example.servers): allow server stop from tests
    
    remove waitForSelectors and ensure service worker response is done before checking for text
    
    debugging: query /view directly
    
    this removes the need for playwright navigating iframes and still utilizes the serviceWorker in the test
    
    * chore: cleanup browser-service-worker test
    
    * Revert "chore(tmp): disable all but browser-service-worker tests"
    
    This reverts commit 76a94cccc775e4ab6ac44c0303290701c423b79e.
    
    * chore: remove test-pr-workflows from push target

--- examples/browser-service-worker/tests/test.js
+++ examples/browser-service-worker/tests/test.js
@@ -1,3 +1,4 @@
+// @ts-check
 import { test, expect } from '@playwright/test';
 import { playwright } from 'test-util-ipfs-example';
 
@@ -10,30 +11,57 @@ play.describe('browser service worker:', () => {
   // DOM
   const linkDOM = "a"
   const textDOM = "body"
-  const debugDOM = "#debug"
 
   play.beforeEach(async ({servers, page}) => {
     await page.goto(`http://localhost:${servers[0].port}/`);
   })
 
-  play('should properly load the content of an IPFS hash', async ({ page }) => {
-    await page.waitForSelector(textDOM)
-    await page.waitForSelector(linkDOM)
+  play('should properly load the content of an IPFS hash', async ({ servers, page, context }) => {
+    page.on('console', msg => console.log('PAGE LOG:', msg.text()));
+    page.on("pageerror", (err) => {
+      console.trace(`pageerror: ${err.message}`)
+    })
 
     expect(await page.textContent(textDOM)).toContain("Load content by adding IPFS path to the URL")
     expect(await page.textContent(linkDOM)).toContain("/ipfs/bafy")
 
-    await page.waitForSelector(`${debugDOM}:has-text("SW is ready")`, {
-      state: 'attached'
+    /**
+     * Request /view path directly, as this is still handled by the service worker but doesn't break tests in github CI.
+     * @see https://github.com/ipfs-examples/js-ipfs-examples/blob/master/examples/browser-service-worker/src/service.js#L48-L52
+     * @see https://github.com/ipfs-examples/js-ipfs-examples/pull/527
+     */
+    const ipfsRequestUrl = `http://localhost:${servers[0].port}/view/ipfs/Qmf412jQZiuVUtdgnB36FXFX7xg5V6KEbSJ4dpQuhkLyfD`
+
+    /**
+     * Wait for the service worker to be ready
+     * @see https://playwright.dev/docs/service-workers-experimental#accessing-service-workers-and-waiting-for-activation
+     */
+    await page.evaluate(async () => {
+      const registration = await window.navigator.serviceWorker.getRegistration();
+      if (registration?.active?.state === 'activated') {
+        console.log('Service worker is already activated')
+        return;
+      }
+      await /** @type {Promise<void>} */(new Promise(res => {
+        window.navigator.serviceWorker.addEventListener('controllerchange', () => {
+          console.log('Service worker is activated')
+          res()
+        })
+      }));
+    });
+
+    const serviceWorkerResponsePromise = new Promise((resolve) => {
+      context.on('response', async (response) => {
+        if (response.url() === ipfsRequestUrl && response.fromServiceWorker()) {
+          resolve(response);
+        }
+      })
     })
 
-    const currentURL = await page.url();
-    await page.goto(`${currentURL}ipfs/Qmf412jQZiuVUtdgnB36FXFX7xg5V6KEbSJ4dpQuhkLyfD`);
-    await page.waitForSelector(textDOM)
+    await page.goto(ipfsRequestUrl, {waitUntil: 'commit'});
+    const serviceWorkerResponse = await serviceWorkerResponsePromise
 
-    const elementFrame = await page.waitForSelector("iframe")
-    const frame = await elementFrame.contentFrame()
-
-    expect(await frame.textContent('pre')).toContain("hello world")
+    expect(await serviceWorkerResponse.status()).toBe(200)
+    expect(await serviceWorkerResponse.text()).toContain("hello world")
   });
 });
